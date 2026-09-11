2de1f0c36cc6c19e3ef4d47dc13b0f9ea7236b12
Kim Gyeongjae <skyneon77@gmail.com>
2026-09-11 17:23:37 +0900

    AABB 함수 분리 및 코드 정리

--- Source/Runtime/Engine/FRayCastingManager.cpp
+++ Source/Runtime/Engine/FRayCastingManager.cpp
@@ -6,6 +6,7 @@
 #include "Runtime/CoreUObject/UPrimitiveComponent.h"
 #include "Runtime/Core/Log.h"
 
+constexpr float Epsilon = 0.000001f;
 
 FRay FRayCastingManager::CreateRayFromScreenPosition(const FCamera& Camera, const FVector2& MousePosition, const FVector2& ViewportSize)
 {
@@ -78,6 +79,45 @@ bool FRayCastingManager::RayIntersectsMeshes(
 	return ClosestComponent != nullptr;
 }
 
+bool FRayCastingManager::RayIntersectsAABB(const FRay& Ray, const FAxisAlignedBoundingBox& AABB)
+{
+	// AABB 판별
+	// Source: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes//ray-box-intersection.html
+	// Source: https://gist.github.com/DomNomNom/46bb1ce47f68d255fd5d
+
+	FVector TMin;
+	FVector TMax;
+	for (int i = 0; i < 3; ++i)
+	{
+		if (std::abs(Ray.Direction[i]) < Epsilon)
+		{
+			if (Ray.Origin[i] < AABB.Min[i] && Ray.Origin[i] > AABB.Max[i])
+			{
+				return false;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		else
+		{
+			TMin[i] = (AABB.Min[i] - Ray.Origin[i]) / Ray.Direction[i];
+			TMax[i] = (AABB.Max[i] - Ray.Origin[i]) / Ray.Direction[i];
+		}
+	}
+
+	float TNear = 0.0f;
+	float TFar = std::numeric_limits<float>::max();
+	for (int i = 0; i < 3; ++i)
+	{
+		TNear = std::max(TNear, std::min(TMin[i], TMax[i]));
+		TFar = std::min(TFar, std::max(TMin[i], TMax[i]));
+	}
+
+	return TNear <= TFar;
+}
+
 bool FRayCastingManager::RayIntersectsMesh(const FRay& Ray, const FMesh& Mesh, const FMatrix& ModelMatrix, float& OutDistance, FVector& OutImpactPoint)
 {
 	const auto& Positions = Mesh.GetPositions();
@@ -87,9 +127,6 @@ bool FRayCastingManager::RayIntersectsMesh(const FRay& Ray, const FMesh& Mesh, c
 	{
 		return false;
 	}
-
-	// AABB
-	FAxisAlignedBoundingBox AABB = Mesh.GetLocalBounds();
 	
 	// Ray를 Object 좌표계로 변환
 	FMatrix InvM;
@@ -99,32 +136,14 @@ bool FRayCastingManager::RayIntersectsMesh(const FRay& Ray, const FMesh& Mesh, c
 	}
 
 	const FVector ObjectOrigin = InvM.TransformPointRow(Ray.Origin);
-	const FVector ObjectDirection = InvM.TransformPointRow(Ray.Direction, 0.0f);
+	const FVector ObjectDirection = InvM.TransformPointRow(Ray.Direction, 0.0f); // 1.0은 점을 나타내므로 0.0으로 하여 벡터로 유지
 	const FRay ObjectRay{ ObjectOrigin, ObjectDirection };
-	
-	// AABB 판별. 자세한 설명은 공부중....
-	float TXMin = (AABB.Min.X - ObjectOrigin.X) / ObjectDirection.X;
-	float TXMax = (AABB.Max.X - ObjectOrigin.X) / ObjectDirection.X;
 
-	if (TXMin > TXMax) { std::swap(TXMin, TXMax); }
-
-	float TYMin = (AABB.Min.Y - ObjectOrigin.Y) / ObjectDirection.Y;
-	float TYMax = (AABB.Max.Y - ObjectOrigin.Y) / ObjectDirection.Y;
-
-	if (TYMin > TYMax) { std::swap(TYMin, TYMax); }
-	if ((TXMin > TYMax) || TYMin > TXMax) { return false; }
-
-	if (TYMin > TXMin) { TXMin = TYMin; }
-	if (TYMax < TXMax) { TXMax = TYMax; }
-
-	float TZMin = (AABB.Min.Z - ObjectOrigin.Z) / ObjectDirection.Z;
-	float TZMax = (AABB.Max.Z - ObjectOrigin.Z) / ObjectDirection.Z;
-
-	if (TZMin > TZMax) { std::swap(TZMin, TZMax); }
-	if (TXMin > TZMax || TZMin > TXMax) { return false; }
-
-	if (TZMin > TXMin) { TXMin = TZMin; }
-	if (TZMax < TXMax) { TXMax = TZMax; }
+	FAxisAlignedBoundingBox AABB = Mesh.GetLocalBounds();
+	if (!RayIntersectsAABB(ObjectRay, AABB))
+	{
+		return false;
+	}
 
 	const uint32 elementCount = Mesh.HasIndices()
 		? static_cast<uint32>(Indices.size())
@@ -174,7 +193,6 @@ bool FRayCastingManager::RayIntersectsTriangle(
 	const FVector& C,
 	float& OutT)
 {
-	constexpr float Epsilon = 0.000001f;
 
 	const FVector edge1 = B - A;
 	const FVector edge2 = C - A;
