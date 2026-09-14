b94179f966661602d88b670b462c7bbeeac70376
Kim Gyeongjae <skyneon77@gmail.com>
2026-09-14 12:06:37 +0900

    fix: TexturedPS 조명 수정

--- Shader/TexturedPS.hlsl
+++ Shader/TexturedPS.hlsl
@@ -18,18 +18,18 @@ float4 MainPS(PS_INPUT Input) : SV_Target
     // 투명 및 어두운 영역 제거
     clip(Sampled.a - 0.1f);
     clip(max(Sampled.r, max(Sampled.g, Sampled.b)) - 0.05f);
-
-    // 정점 색상 반영
-    float3 TintedColor = Sampled.rgb * Input.Color.rgb;
-    float3 BaseColor = lerp(TintedColor, ColorOverride, ColorOverrideAmount);
-
-    // 태양광 계산
+    
+    // 색상 조정
+    float3 BaseColor = Sampled.rgb * ColorOverride * ColorOverrideAmount;
+    
+    // ==== Directional Light 계산 ====
     float3 N = normalize(Input.Normal);
     float NdotL = max(0.0f, dot(N, -normalize(LightDirection)));
-
     float3 Diffuse = LightColor * (Intensity * NdotL);
     float3 Ambient = LightColor * AmbientIntensity;
+    float3 DirectionalLight = Ambient + Diffuse;
+    // ================================
 
-    float3 FinalColor = BaseColor * (Ambient + Diffuse);
+    float3 FinalColor = BaseColor * DirectionalLight;
     return float4(FinalColor, Sampled.a);
 }
